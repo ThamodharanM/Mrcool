3d68fa578dee3deff9160588a0e26359d2a42089
GITHUB <thamokingdom@gmail.com>
2022-08-27 15:03:06 +0530

    thamo

---
+++ src/test/java/org/test/Git.java
@@ -0,0 +1,15 @@
+package org.test;
+
+import org.checkerframework.checker.units.qual.m2;
+
+public class Git {
+	public void m1() {
+		System.out.println(10);
+	}
+	private void m2() {
+System.out.println(20);
+	}
+private void m3() {
+System.out.println(30);
+}
+}
